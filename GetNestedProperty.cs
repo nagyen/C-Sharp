937a07896decb4d9e8e121ccc36fed0bfe0bf64c
nagarjuna yendluri <nagyen@users.noreply.github.com>
2015-07-28 12:18:12 -0400

    Update GetNestedProperty.cs

--- GetNestedProperty.cs
+++ GetNestedProperty.cs
@@ -1,14 +1,20 @@
-//helper to get nested property
-        public static PropertyInfo GetProp<T>(String name)
-            where T:new()
-        {
-            PropertyInfo info = null;
-            Type type = typeof(T);
-            foreach (String part in name.Split('.'))
-            {
-                info = type.GetProperty(part);
-                if (info == null) { return null; }
-                type = info.PropertyType;
-            }
-            return info;
-        }
+//helper to get nested property 
+public static PropertyInfo GetNestedProperty<T>(String rootPropertyName)
+    where T:new()
+{
+    PropertyInfo info = null;
+    //get type of Generic class
+    Type type = typeof(T);
+    //split the string by '.' and iterate untill last child property
+    foreach (String part in rootPropertyName.Split('.'))
+    {
+        //get property info
+        info = type.GetProperty(part);
+        //if no info return
+        if (info == null) { return null; }
+        //if info exists store it as new type and repeat
+        type = info.PropertyType;
+    }
+    //return propertinfo of child property
+    return info;
+}
