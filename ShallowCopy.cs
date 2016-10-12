631a6733c4e7124d08b91fc642973bab09105a3e
nagarjuna yendluri <nagyen@users.noreply.github.com>
2016-10-12 15:15:53 -0400

    Update ShallowCopy.cs

--- ShallowCopy.cs
+++ ShallowCopy.cs
@@ -1,7 +1,7 @@
 //extension method to do a shallow copy of object
 public class BaseClass
 {
-  public T ShallowCopy<T>() where T : BaseClass
+  public T ShallowCopy<T>() where T : Class
   {
       return (T)(MemberwiseClone());
   }
